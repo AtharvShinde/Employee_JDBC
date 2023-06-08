1a36c34a473591d49cf9c9b415b74518adaf5822
Atharv Shinde <63412033+AtharvShinde@users.noreply.github.com>
2023-06-08 20:46:14 +0530

    Update Main.java

--- src/com/programming/empapp/Main.java
+++ src/com/programming/empapp/Main.java
@@ -12,13 +12,12 @@ public class Main {
         do{
             System.out.println("1. Add Employee \n"+
                     "2. Show All Employee \n"+
-                            "3. Show Employee based on id \n"+
+                            "3. Show Employee based on ID \n"+
                             "4. Update the Employee \n"+
                             "5. Delete the Employee \n");
-            System.out.println("Enter your choice");
+            System.out.println("Enter Your Choice");
             int c=sc.nextInt();
             switch (c){
-
                 case 1:
                     Employee emp=new Employee();
                     System.out.print("Enter ID: ");
@@ -39,27 +38,27 @@ public class Main {
                     dao.showAllEmployee();
                     break;
                 case 3:
-                    System.out.println("Enter id to show the details");
+                    System.out.println("Enter ID To Show The Details");
                     int empid=sc.nextInt();
                     dao.showEmployeeBasedOnID(empid);
                     break;
                 case  4:
-                    System.out.println("Enter id to update the details");
+                    System.out.println("Enter ID To Update The Details");
                     int empid1=sc.nextInt();
-                    System.out.println("Enter the new name");
+                    System.out.println("Enter The New Name");
                     name=sc.next();
                     dao.updateEmployee(empid1,name);
                     break;
                 case 5:
-                    System.out.println("Enter the id to delete");
+                    System.out.println("Enter The ID To Delete");
                     id=sc.nextInt();
                     dao.deleteEmployee(id);
                     break;
                 case 6:
-                    System.out.println("Thankyou for using the Application");
+                    System.out.println("Thank You For Using Our Application");
                     break;
                 default:
-                    System.out.println("Enter valid choice");
+                    System.out.println("Invalid Choice //Enter Valid Choice");
                     break;
             }
         }while (true);
