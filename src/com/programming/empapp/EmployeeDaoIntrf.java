5d6628eae6e0bdd3cffe74bff43aa1fc5e713d3c
Atharv Shinde <63412033+AtharvShinde@users.noreply.github.com>
2023-06-08 20:44:15 +0530

    Update EmployeeDaoIntrf.java

--- src/com/programming/empapp/EmployeeDaoIntrf.java
+++ src/com/programming/empapp/EmployeeDaoIntrf.java
@@ -1,16 +1,9 @@
 package com.programming.empapp;
 
 public interface EmployeeDaoIntrf {
-    //create employee
-    //show all emp
-    //show emp based on id
-    //update emp
-    //delete emp
-
     public void createEmployee(Employee emp);
     public void showAllEmployee();
     public void  showEmployeeBasedOnID(int id);
     public void  updateEmployee(int id, String name);
     public void deleteEmployee(int id);
-
 }
